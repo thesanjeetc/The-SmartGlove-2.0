14616ab564745eb1d20017cdc28138d943b5bed8
theraspberryguy <21122782+theraspberryguy@users.noreply.github.com>
2020-02-17 20:09:44 +0000

    Added Merge Sort

--- Server/Utils.js
+++ Server/Utils.js
@@ -52,9 +52,47 @@ const wakeUpDyno = () => {
   }, 1500000);
 };
 
+const merge = (left, right, direction) => {
+  let sorted = [];
+  let compare = () => (direction ? left[0] < right[0] : left[0] > right[0]);
+
+  while (left.length > 0 && right.length > 0) {
+    if (compare()) {
+      sorted.push(left.shift());
+    } else {
+      sorted.push(right.shift());
+    }
+  }
+
+  return [...sorted, ...left, ...right];
+};
+
+const mergeSort = (list, direction = true) => {
+  if (list.length < 2) return list;
+
+  let centre = Math.floor(list.length / 2);
+  let left = list.slice(0, centre);
+  let right = list.slice(centre);
+
+  return merge(
+    mergeSort(left, direction),
+    mergeSort(right, direction),
+    direction
+  );
+};
+
+let l = [
+  "02/17/2020 5:45 PM",
+  "02/17/2020 5:39 AM",
+  "02/17/2020 5:35 PM",
+  "02/14/2020 7:49 PM"
+];
+console.log(mergeSort(l, 1));
+
 module.exports = {
   Stream,
   Timer,
   wakeUpDyno,
+  mergeSort,
   UID
 };
